ac8ca6e137d8afd0f1f436c9e91f19aa649c1d7f
Ana Giordano <giordano@us.ibm.com>
2017-04-10 07:48:28 -0500

    debug

--- getItems.php
+++ getItems.php
@@ -3,19 +3,24 @@
 function RetrieveItems()
 {
     echo "\r\n**************************************";
+    console.log("Log: ", "*****************");
     $application = getenv("VCAP_APPLICATION");
     echo "\r\napplication:" . $application;
+    console.log("Application: ", $application);
     $application_json = json_decode($application, true);
     $applicationURI = $application_json["application_uris"][0];
     echo "\r\napplicationURI:" . $applicationURI;
+    console.log("Application URI: ", $applicationURI);
     if (substr( $applicationURI, 0, 3 ) === "ui-") {
         $catalogHost = "catalog-api-" . substr($applicationURI, 3);
     } else {
         $catalogHost = str_replace("-ui-", "-catalog-api-", $applicationURI);
     }
     echo "\r\ncatalogHost:" . $catalogHost;    
+    console.log(">>>>>>>>>>>>>>Catalog Host:", $catalogHost);
     $catalogRoute = "http://" . $catalogHost;
-    echo "\r\ncatalogRoute:" . $catalogRoute;    
+    echo "\r\ncatalogRoute:" . $catalogRoute;  
+    console.log(">>>>>>>>>>>>>>Catalog Route", $catalogRoute);
     $url = $catalogRoute . "/items";
 
     $curl = curl_init();
